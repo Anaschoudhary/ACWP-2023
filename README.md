00faaab675f5543eb0f7e177439e15a40c2f6719
Anas Choudhary <anas9719@gmail.com>
2023-05-02 17:55:40 +0530

    acwp2023 First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Wordpress Theme - ACWP 2023
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main Template File
+ * @package acwp2023
+*/
+?>
+<!DOCTYPE html>  
+<html>  
+    <head>  
+        <meta charset="UTF-8">  
+        <meta name="description" content="Wordpress Theme ACWP 2023">  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    </head>  
+    <body>  
+        <p>ACWP Theme 2023</p>  
+    </body>  
+</html>  
